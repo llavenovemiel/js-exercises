76572c08d490cd127a96f78b88210abe2a9e8f7d
Novemiel Llave <42906060+llavenovemiel@users.noreply.github.com>
2018-11-13 09:20:23 +0800

    Update flatten.js

--- flatten.js
+++ flatten.js
@@ -1,5 +1,5 @@
 const flatten = (arr) => {
 	let flat = [];
-	arr.forEach(el => flat = flat.concat(Array.isArray(el) ? flatten(el) : el);
+	arr.forEach(el => flat = flat.concat(Array.isArray(el) ? flatten(el) : el));
 	return flat;
 }
